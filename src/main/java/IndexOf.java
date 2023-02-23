9d5785535d30c484d48e164184f44fd7679128ef
lau-phu <plau123@ymail.com>
2023-02-23 15:05:08 +0000

    dasdasdasd

--- src/main/java/IndexOf.java
+++ src/main/java/IndexOf.java
@@ -11,6 +11,11 @@ public class IndexOf {
      * @return the first index (position) of n in arr. if n does not exist in arr, return -1.
      */
     public int getIndex(int[] arr, int n){
+        int a = 0;
+        for(int i:arr){
+            if(i == n) return a;
+            a++;
+        }
         return -1;
     }
 }
